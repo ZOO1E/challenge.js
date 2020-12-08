abf47dc19be561f482baccb0d77dee45f4bedb32
ZOO1E <73783859+ZOO1E@users.noreply.github.com>
2020-12-08 20:21:52 +0900

    Add files via upload

---
+++ challenge#1.js
@@ -0,0 +1,24 @@
+
+//const JohnMass = '92';
+// const JohnHeight = '1.95';
+// const MarkMass = '78';
+// const MarkHeight = '1.69';
+
+const JohnMass = '85';
+const JohnHeight = '1.76';
+
+const MarkMass = '95';
+const MarkHeight = '1.88';
+
+let JohnBMI = JohnMass/ JohnHeight ** 2 
+let JohnBMI2 = JohnMass / (JohnHeight * JohnHeight)
+const JBMI = JohnBMI = JohnBMI2;
+
+
+let MarkBMI =  MarkMass/ MarkHeight ** 2 
+let MarkBMI2 = MarkMass / (MarkHeight * MarkHeight)
+const MBMI = MarkBMI = MarkBMI2;
+
+
+const markHigherBMI =  MBMI > JBMI;
+console.log(MBMI, JBMI , markHigherBMI);
